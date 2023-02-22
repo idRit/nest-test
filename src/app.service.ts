7b33e2bbe1a281eaf811deac2480d7abdefaed89
Ritwik Mukherjee <ritwik.mukherjee@equentis.com>
2023-02-22 17:15:28 +0530

    chore: init

---
+++ src/app.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+
+import * as winston from 'winston';
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    //
+    // - Write to all logs with level `info` and below to `combined.log` 
+    // - Write all logs error (and below) to `error.log`.
+    //
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' }),
+  ],
+});
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    logger.info('Hello World!');
+    return 'Hello World!';
+  }
+}
